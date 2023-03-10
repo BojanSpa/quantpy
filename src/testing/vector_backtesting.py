Fixed bug with return calculation

src/testing/vector_backtesting.py
@@ -20,8 +20,8 @@ class VectorStrategyReport:
     params: str
     symbol: str
     tf: str
-    returns: float
-    strat_returns: float
+    cum_returns: float
+    cum_strat_returns: float
     equity: float
 
 
@@ -30,18 +30,18 @@ class VectorStrategy:
     args: str
 
     def run(self, data):
-        pass
+        self.__clear(data)
 
-    def calc_returns(self, data):
-        data['returns'] = np.log(data.close / data.close.shift(1))
-        data['creturns'] = data.returns.cumsum().apply(np.exp)
 
     def calc_equity(self, data, init_equity):
-        data['equity'] = data.cum_strat_returns * init_equity
+        data['equity'] = data.cum_strat_return * init_equity
 
     def calc_drawdown(self, data):
         data['max_equity'] = data.equity.cummax()
         data['drawdown'] = (data.max_equity - data.equity) * (-1)
+
+    def __clear(self, data):
+        data.drop(columns=['position', 'strat_return', 'cum_strat_return', 'equity', 'max_equity', 'drawdown'], errors='ignore')
 
 
 class SmaCrossVectorStrategy(VectorStrategy):
@@ -56,7 +56,7 @@ class SmaCrossVectorStrategy(VectorStrategy):
 
 
     def run(self, data, init_equity=1000):
-        super().calc_returns(data)
+        super().run(data)
 
         data['ma_fast'] = data.close.rolling(self.fast).mean()
         data['ma_slow'] = data.close.rolling(self.slow).mean()
@@ -64,8 +64,8 @@ class SmaCrossVectorStrategy(VectorStrategy):
         data['position'] = np.where(data.ma_fast > data.ma_slow, 1, -1)
         data.dropna(inplace=True)
 
-        data['strat_returns'] = data.position.shift(1) * data.returns
-        data['cum_strat_returns'] = data.strat_returns.cumsum().apply(np.exp)
+        data['strat_return'] = data.position.shift(1) * data.log_return
+        data['cum_strat_return'] = data.strat_return.cumsum().apply(np.exp)
 
         super().calc_equity(data, init_equity)
         super().calc_drawdown(data)
@@ -74,7 +74,7 @@ class SmaCrossVectorStrategy(VectorStrategy):
 
 
 class DmiVectorStrategy(VectorStrategy):
-    name = 'DMI'
+    name = 'DM'
 
     def __init__(self, length):
         self.length = length
@@ -83,14 +83,14 @@ class DmiVectorStrategy(VectorStrategy):
 
 
     def run(self, data: pd.DataFrame, init_equity=1000):
-        super().calc_returns(data)
+        super().run(data)
 
         data[['dm_p', 'dm_n']] = pdta.dm(data.high, data.low, self.length)
         data['position'] = np.where(data.dm_p > data.dm_n, 1, -1)
         data.dropna(inplace=True)
 
-        data['strat_returns'] = data.position.shift(1) * data.returns
-        data['cum_strat_returns'] = data.strat_returns.cumsum().apply(np.exp)
+        data['strat_return'] = data.position.shift(1) * data.log_return
+        data['cum_strat_return'] = data.strat_return.cumsum().apply(np.exp)
 
         super().calc_equity(data, init_equity)
         super().calc_drawdown(data)
@@ -127,8 +127,8 @@ class VectorBacktester:
             strategy.args,
             symbol,
             timeframe,
-            lrow.creturns, 
-            lrow.cum_strat_returns,
+            lrow.cum_return, 
+            lrow.cum_strat_return,
             lrow.equity)
 
 